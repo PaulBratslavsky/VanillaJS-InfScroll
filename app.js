31e813497f0058f734993600eeb75ac7db908ca4
John Connor <johnconnor@Johns-MacBook-Pro.local>
2020-09-05 17:47:30 -0500

    load on scroll finished

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
-
-console.log("Ready");
-
 /********************************************
     GLOBAL VARIABLES
 ********************************************/
+const loader = document.getElementById('loader-container');
+let count = 2;
+let skip = 0;
 let dataArray = [];
+
+let isReady = false;
+let videosLoaded = 0;
+let totalVideos = 0;
+
+
 
 /********************************************
     FUNCTIONS
 ********************************************/
+function handleOnLoadVideo() {
+    videosLoaded++;
+    console.log(videosLoaded, 'vs', totalVideos)
+    if (videosLoaded === totalVideos) {
+        isReady = true;
+        loader.hidden = true;
+    } 
+}
+
 async function getVideosFromAPI() {
     const urlBase = 'https://scrapiapi.herokuapp.com/' 
     const apiPoint = 'videos';
-    
+    const limit = '&_limit=';
+    const start = '?_start=';
+
     try {
-        dataArray = await fetch(urlBase + apiPoint).then(data => data.json());
-        console.log(dataArray);
+        result = await fetch(urlBase + apiPoint + start + skip + limit + count)
+        data =  await result.json();
+        totalVideos = data.length;
+        renderVideosToDOM(data);
+        skip = skip + count;
     } catch (err) {
         console.error("APP ERROR ", err);
     }
@@ -28,47 +48,74 @@ function getId(url) {
     return match && match[2].length === 11 ? match[2] : null;
 }
 
-function renderVideosToDOM() {
+function renderVideosToDOM(arrayData) {
+    videosLoaded = 0;
     const imageContainer = document.getElementById('container');
-    const loader = document.getElementById('loader-container');
-
-    imageContainer.innerHTML = generateVideoHTML('test','hello','61P0yKsU8yw');
+    arrayData.forEach(item => imageContainer.appendChild(generateVideoHTML(item.Title, item.Desription, getId(item.Url), handleOnLoadVideo)));
 }
 
 /********************************************
     HTML TEMPLATE
 ********************************************/
-function generateVideoHTML(title, description, id) {
-    return `
-        <div class="video-container ">
-            <div class="image-description">
-                <div><h2>${title}</h2></div>
-                <div><p>${description}</p></div>
-            </div>
-            <iframe width="560" height="349" src="http://www.youtube.com/embed/${id}" frameborder="0" allowfullscreen></iframe>
-        </div>
-    `
+function generateVideoHTML(title, description, id, callbackFunction) {
+    const videoContainer = document.createElement('div');
+    videoContainer.className = "video-container";
+
+    const videoDescription = document.createElement('div');
+    videoDescription.className = "video-description";
+
+    const videoTitleContainer = document.createElement('div');
+    const videoTitleText = document.createElement('h2');
+    videoTitleText.innerText = title;
+
+    const videoDescriptionContainer = document.createElement('div');
+    const videoDescriptionText = document.createElement('p');
+    videoDescriptionText.innerText = description;
+
+    const video = document.createElement('iframe');
+    video.setAttribute('src', `http://www.youtube.com/embed/${id}`);
+    video.setAttribute('frameborder', '0');
+
+    video.addEventListener('load', callbackFunction);
+
+    videoTitleContainer.appendChild(videoTitleText);
+    videoDescriptionContainer.appendChild(videoDescriptionText);
+
+    videoDescription.appendChild(videoTitleContainer);
+    videoDescription.appendChild(videoDescriptionContainer);
+
+    videoContainer.appendChild(videoDescription);
+    videoContainer.appendChild(video);
+
+    return videoContainer;   
 }
 
+// function generateVideoHTML(title, description, id) {
+//     return `
+//         <div class="video-container">
+//             <div class="video-description">
+//                 <div><h2>${title}</h2></div>
+//                 <div><p>${description}</p></div>
+//             </div>
+//             <iframe width="560" height="349" src="http://www.youtube.com/embed/${id}" frameborder="0" allowfullscreen></iframe>
+//         </div>
+//     `
+// }
+
+/********************************************
+    EVENT LISTENER
+********************************************/
+window.addEventListener('click', () => getVideosFromAPI());
+
+window.addEventListener('scroll', () => {
+    console.log('scroll', isReady);
+    if (window.innerHeight + window.screenY >= document.body.offsetHeight - 1500 && isReady) {
+        getVideosFromAPI();
+        isReady = false;
+    }    
+})
 /********************************************
     FUNCTIONS CALL
 ********************************************/
 getVideosFromAPI();
-renderVideosToDOM();
-console.log(dataArray);
 
-/********************************************
-    EXAMPLE CODE
-********************************************/
-
-// async function getPhotosFromAPI() {
-//     const COUNT = 15;
-//     const YOUR_ACCESS_KEY = '2aWbj014bYBiOPNzwlqXCPEzIwa6pjIV4BZLpmRxQkA';
-//     const URL_BASE = `https://api.unsplash.com/photos/random/?client_id=${YOUR_ACCESS_KEY}&count=${COUNT}`;
-
-//     try {
-//         const photosDataArray = await fetch(URL_BASE).then(data => data.json());
-//     } catch (err) {
-//         console.error("API ERROR: ", err);    
-//     }
-// }
